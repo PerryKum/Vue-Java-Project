ece0b682e0153ca60d83589b8dd49ba02e2c1eca
PerryKum <2273157479@qq.com>
2024-04-20 17:43:34 +0800

    update

---
+++ ruoyi-ui/src/components/RuoYi/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RuoYiDoc',
+  data() {
+    return {
+      url: 'http://vue_blog-tqei477i-perrykum.4everland.app/'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
